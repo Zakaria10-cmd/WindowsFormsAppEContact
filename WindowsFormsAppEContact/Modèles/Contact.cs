4a54206ab77b4d9c6c9aafc046483e6aefffaf63
ZAK <ZAK@ZAKARIA-PC>
2020-05-06 09:00:05 +0100

    Envoi après Modif

---
+++ WindowsFormsAppEContact/Modèles/Contact.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppEContact.Modèles
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Nomcomplet { get; set; }
+            public DateTime Datenaiss { get; set; }
+       
+           public string Email { get; set; }
+       
+           public string Tel { get; set;}
+       
+          public string Genre { get; set; }
+
+        public string UrlPhoto { get; set; }
+
+        public Contact(string nomcomplet, DateTime datenaiss, string email, string tel, string genre, string urlPhoto)
+        {
+            Nomcomplet = nomcomplet;
+            this.Datenaiss = datenaiss;
+            this.Email = email;
+            this.Tel = tel;
+            this.Genre = genre;
+            UrlPhoto = urlPhoto;
+
+        }
+
+        public Contact()
+
+
+    }
+}
